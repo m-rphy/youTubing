3028d1b307fa19e7c16eed401937bb1cc3fda00f
William Murphy <w.williamjmurphy@gmail.com>
2023-08-21 21:42:05 -0600

    added some error handling

--- video-processing-service/src/controllers/gcsController.ts
+++ video-processing-service/src/controllers/gcsController.ts
@@ -1,19 +1,38 @@
 import { Request, Response, NextFunction } from "express";
-import { downloadRawVid, uploadProcVid } from "./gcsControllerHelperFuncs";
+import { deletePrcVideo, deleteRawVideo, downloadRawVid, uploadProcVid } from "./gcsControllerHelperFuncs";
 
 export const gcsController = {
     download: async (_: Request, res: Response, next: NextFunction) => {
-
-        const {inputFileName } = res.locals;
-        await downloadRawVid(inputFileName);
-        return next()
+        
+        const {outputFileName, inputFileName } = res.locals;
+        
+        try {
+           await downloadRawVid(inputFileName);
+           return next()
+        } catch (err) {
+           await Promise.all([
+               deletePrcVideo(outputFileName),
+               deleteRawVideo(inputFileName)
+           ]);
+           return next(new Error('Error in gcsController.download'));
+        }
     },
 
     upload: async (_: Request, res: Response, next: NextFunction) => {
 
-        const { outputFileName} = res.locals;
-        await uploadProcVid(outputFileName);
-        return next()
+        const {outputFileName, inputFileName } = res.locals;
+        
+        try {
+           const { outputFileName} = res.locals;
+           await uploadProcVid(outputFileName);
+           return next()
+        } catch (err) {
+           await Promise.all([
+               deletePrcVideo(outputFileName),
+               deleteRawVideo(inputFileName)
+           ]);
+           return next(new Error('Error in gcsController.upload'));
+        }
     },
     
 }
